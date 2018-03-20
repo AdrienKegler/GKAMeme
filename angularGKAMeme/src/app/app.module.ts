c48fd54c5daa42932f7f2e54631242ce16037b7f
AdrienKegler <adrien.kegler@viacesi.fr>
2018-03-20 23:48:58 +0100

    Sign In overlay

--- angularGKAMeme/src/app/app.module.ts
+++ angularGKAMeme/src/app/app.module.ts
@@ -1,36 +1,38 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from "@angular/router";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 
 import { AppComponent } from './app.component';
+import { BtnGroupSearchbarComponent } from './navbar/btn-group-searchbar/btn-group-searchbar.component';
 import { FileuploaderComponent } from './fileuploader/fileuploader.component';
 import { HeaderComponent } from './header/header.component';
-import { NavBarComponent } from './navbar/navbar.component';
 import { IndexComponent } from './index/index.component';
+import { NavBarComponent } from './navbar/navbar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { UserLoggedRightPartNavBarComponent } from './navbar/user-logged-right-part-nav-bar/user-logged-right-part-nav-bar.component';
 import { VisitorRightPartNavBarComponent } from './navbar/visitor-right-part-nav-bar/visitor-right-part-nav-bar.component';
-import { BtnGroupSearchbarComponent } from './navbar/btn-group-searchbar/btn-group-searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BtnGroupSearchbarComponent,
     FileuploaderComponent,
     HeaderComponent,
-    NavBarComponent,
     IndexComponent,
+    NavBarComponent,
     SignInComponent,
     UserLoggedRightPartNavBarComponent,
-    VisitorRightPartNavBarComponent,
-    BtnGroupSearchbarComponent
+    VisitorRightPartNavBarComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    HttpClientModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: "index", component:IndexComponent},
       {path: "SignIn", component:SignInComponent}
